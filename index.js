cd0683da3e28f9550869d0ff3b2e2e30766e26e2
enrike7lg <enri7luk@hotmail.com>
2021-03-19 17:39:46 +0100

    index change info

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get("/cool", (request,response) => {
 	console.log("New request to /cool has arrived");
 })//cada vez que se hace un get por http 
 
+app.get("/info/children-out-school", (req, res) => {
+    res.send("<html><body><table class = default> <tr> <th>country</th>  <th>year</th> <th>children-out-school-male </th> <th>children-out-school-female </th> <th>children-out-school-total </th> </tr>   <tr>    <td>Argentina</td>   <td>1970 </td>  <td>94757 </td>   <td>61145 </td>  <td>155902 </td>  </tr> <tr>  <th>Switzerland </th> <th>2019 </th><th>195.93 </th>	 <th>129.7 </th>  <th>78.5 </th> </tr> <tr><th>Finland </th><th>2019 </th> <th>194.01 </th>	<th>112.3 </th> <th>77.2 </th></tr>   <tr>  <th>Australia </th>	   <th>2019 </th>     <th>191.13 </th>       <th>122.85 </th>     <th>57.24 </th>  </tr>        <tr>  <th>Austria </th>  	<th>2019 </th>  <th>191.05 </th>  	<th>96.7 </th>   <th>78.63 </th></tr> </table> </body> </html>");
+});
+
 app.listen(port,() => {
 	console.log("Server already listening on port " + port);
 });
