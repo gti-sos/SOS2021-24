560cce0bc51309f11a9b1853ad72cb95093bbc85
enrike7lg <enri7luk@hotmail.com>
2021-03-19 17:46:54 +0100

    index change info

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/cool", (request,response) => {
 })//cada vez que se hace un get por http 
 
 app.get("/info/children-out-school", (req, res) => {
-    res.send("<html><body><table class = default> <tr> <th>country</th>  <th>year</th> <th>children-out-school-male </th> <th>children-out-school-female </th> <th>children-out-school-total </th> </tr>   <tr>    <td>Argentina</td>   <td>1970 </td>  <td>94757 </td>   <td>61145 </td>  <td>155902 </td>  </tr> <tr>  <th>Switzerland </th> <th>2019 </th><th>195.93 </th>	 <th>129.7 </th>  <th>78.5 </th> </tr> <tr><th>Finland </th><th>2019 </th> <th>194.01 </th>	<th>112.3 </th> <th>77.2 </th></tr>   <tr>  <th>Australia </th>	   <th>2019 </th>     <th>191.13 </th>       <th>122.85 </th>     <th>57.24 </th>  </tr>        <tr>  <th>Austria </th>  	<th>2019 </th>  <th>191.05 </th>  	<th>96.7 </th>   <th>78.63 </th></tr> </table> </body> </html>");
+    res.send("<html><body><table class = default><caption>Children out of school by year and country</caption> <tr> <th>country</th>  <th>year</th> <th>children-out-school-male </th> <th>children-out-school-female </th> <th>children-out-school-total </th> </tr>   <tr>    <th>Argentina</th>   <th>1970 </th>  <th>94757 </th>   <th>61145 </th>  <th>155902 </th>  </tr> <tr>  <th>Italy </th> <th>1976 </th><th>55165 </th>	 <th>18496 </th>  <th>73661 </th> </tr> <tr><th>Spain </th><th>2018 </th> <th>50956 </th>	<th>40830 </th> <th>91786 </th></tr>   <tr>  <th>France </th>	   <th>2019 </th>     <th>191.13 </th>       <th>122.85 </th>     <th>57.24 </th>  </tr>        <tr>  <th>Austria </th>  	<th>2019 </th>  <th>191.05 </th>  	<th>96.7 </th>   <th>78.63 </th></tr> </table> </body> </html>");
 });
 
 app.listen(port,() => {
