4ff70ba7c8ee971f52938b278de15e61e64a2211
enrike7lg <enri7luk@hotmail.com>
2021-04-16 18:37:11 +0200

    yokse

--- schoolAPI/index.js
+++ schoolAPI/index.js
@@ -255,10 +255,10 @@ app.put(BASE_CHILDREN_OUT_SCHOOL_API_PATH + "/:country/:year", (req, res) => {
 });
     
 //POST: Post a un recurso -> error método no permitido
-    app.post(BASE_CHILDREN_OUT_SCHOOL_API_PATH + "/:country/:year", (req, res) => {
-        console.log("Method not allowed");
-        res.sendStatus(405);
-      })
+app.post(BASE_CHILDREN_OUT_SCHOOL_API_PATH + "/:country/:year", (req, res) => {
+    console.log("Method not allowed");
+     res.sendStatus(405);
+})
     
 //PUT: Put a la lista de recursos -> debe dar un error de método no permitido
     app.put(BASE_CHILDREN_OUT_SCHOOL_API_PATH, (req, res) => {
