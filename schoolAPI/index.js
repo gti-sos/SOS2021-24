f62606b540659a1c604a090b7962f075c475402d
enrike7lg <enri7luk@hotmail.com>
2021-04-17 12:35:42 +0200

    Query fix

--- schoolAPI/index.js
+++ schoolAPI/index.js
@@ -74,7 +74,7 @@ app.get(BASE_CHILDREN_OUT_SCHOOL_API_PATH  + "/loadInitialData", (req, res) => {
       
 //GET children-out-school Devuelve la lista de recursos (array JSON) w/ query
 app.get(BASE_CHILDREN_OUT_SCHOOL_API_PATH, (req,res)=>{
-        console.log("New GET .../children-out-school");
+    console.log("New GET .../children-out-school");
 
         var query = req.query;
 
