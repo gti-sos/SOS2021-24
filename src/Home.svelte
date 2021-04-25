9e23df97b1e6b26f8f157e46a708adbc359faf7b
enrike7lg <enri7luk@hotmail.com>
2021-04-25 16:46:03 +0200

    svelte fix

--- src/Home.svelte
+++ src/Home.svelte
@@ -58,26 +58,3 @@
             </li>
         </ul>
 </main>
-
-<style>
-    main {
-      text-align: center;
-      padding: 1em;
-      max-width: 240px;
-      margin: 0 auto;
-    }
-    h1 {
-      color: #ff3e00;
-      text-transform: uppercase;
-      font-size: 4em;
-      font-weight: 100;
-    }
-    b {
-      color: #00c1ff;
-    }
-    @media (min-width: 640px) {
-      main {
-        max-width: none;
-      }
-    }
-  </style>
