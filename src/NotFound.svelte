dfedcdea3bec62fe49c81ce4182fd0b900ca395d
enrike7lg <enri7luk@hotmail.com>
2021-04-25 16:11:59 +0200

    svelte first steps

---
+++ src/NotFound.svelte
@@ -0,0 +1,21 @@
+<main>
+    <h2>404</h2>
+  <h4>¡Esta Página no existe!</h4>
+  </main>
+  
+  <style>
+      main {
+      text-align: center;
+      padding: 1em;
+      margin: 0 auto;
+      max-width: 380px;
+    }
+    h2{
+      font-size: 100px;
+    }
+    h4{
+      color: white;
+      padding: 10px;
+      margin: 5px;
+    }
+  </style>
