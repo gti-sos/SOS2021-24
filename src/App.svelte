bead71c1c2582c0f88404ad6c0bf811646849bf6
enrike7lg <enri7luk@hotmail.com>
2021-04-25 16:31:20 +0200

    pruebas

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Router from 'svelte-spa-router';
+	import NotFound from './NotFound.svelte' ;  
+	import Home from './Home.svelte';
+	import { Jumbotron } from "sveltestrap";
+	import { Collapse,Navbar,NavbarToggler,NavbarBrand,Nav,NavItem,NavLink,Dropdown,UncontrolledDropdown,DropdownToggle,DropdownMenu,DropdownItem } from 'sveltestrap';
+
+    const routes = {
+		"/":Home,
+		'*': NotFound};
+</script>
+
+<main>
+    <Router {routes}> </Router>
+</main>
---
+++ src/Home.svelte
@@ -0,0 +1,44 @@
+<main>
+    <head>
+        <link rel='stylesheet' href='/global.css'>
+        <link rel='stylesheet' href='/build/bundle.css'>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    
+    <body>
+        <ul>
+            <li class="has-line-data" data-line-start="2" data-line-end="6">
+            <p class="has-line-data" data-line-start="2" data-line-end="3"><strong>Team</strong></p>
+            <ul>
+            <li class="has-line-data" data-line-start="3" data-line-end="4"><a href="https://github.com/enrike7lg">Enrique Luque García</a></li>
+            <li class="has-line-data" data-line-start="4" data-line-end="5"><a href="https://github.com/framacvaz ">Francisco Javier Macero Vázquez</a></li>
+            <li class="has-line-data" data-line-start="5" data-line-end="6"><a href="https://github.com/juaroltor ">Juan Ignacio Roldán Torres </a></li>
+            </ul>
+            </li>
+            <li class="has-line-data" data-line-start="6" data-line-end="7">
+            <p class="has-line-data" data-line-start="6" data-line-end="7"><strong>Project description</strong>:Correlation between the number of children who drop out of school (primary education), the prevalence of HIV in young people between 0-14 years old and the percentage of employed children between 7-14 years old.</p>
+            </li>
+            <li class="has-line-data" data-line-start="7" data-line-end="8">
+            <p class="has-line-data" data-line-start="7" data-line-end="8"><strong>Repository</strong>: <a href="https://github.com/gti-sos/SOS2021-24">gti-sos/SOS2021-24</a></p>
+            </li>
+            <li class="has-line-data" data-line-start="8" data-line-end="9">
+            <p class="has-line-data" data-line-start="8" data-line-end="9"><strong>URL</strong>: <a href="https://sos2021-24.herokuapp.com">https://sos2021-24.herokuapp.com</a></p>
+            </li>
+            <li class="has-line-data" data-line-start="9" data-line-end="14">
+            <p class="has-line-data" data-line-start="9" data-line-end="10"><strong>APIs</strong>:</p>
+            <ul>
+            <li class="has-line-data" data-line-start="10" data-line-end="11"><a href="https://sos2021-24.herokuapp.com/api/v1/children-out-school">https://sos2021-24.herokuapp.com/api/v1/children-out-school</a> (developed by <a href="https://github.com/enrike7lg">Enrique Luque García</a>)</li>
+            <li class="has-line-data" data-line-start="11" data-line-end="12"><a href="https://sos2021-24.herokuapp.com/api/v1/children-employment">https://sos2021-24.herokuapp.com/api/v1/children-employment</a> (developed by <a href="https://github.com/framacvaz">Francisco Javier Macero Vázquez</a>)</li>
+            <li class="has-line-data" data-line-start="12" data-line-end="14"><a href="https://sos2021-24.herokuapp.com/api/v1/children-with-hiv">https://sos2021-24.herokuapp.com/api/v1/children-with-hiv</a> (developed by <a href="https://github.com/juaroltor">Juan Roldán Torres</a>)</li>
+            </ul>
+            </li>
+            <li class="has-line-data" data-line-start="14" data-line-end="18">
+            <p class="has-line-data" data-line-start="14" data-line-end="15"><strong>Links Postman</strong>:</p>
+            <ul>
+            <li class="has-line-data" data-line-start="15" data-line-end="16">V1 <a href="https://documenter.getpostman.com/view/14937153/TzJoDfw2">SOS2021-24 children-out-school API Doc</a> (developed by <a  href="https://github.com/enrike7lg">Enrique Luque García</a>)</li>
+            <li class="has-line-data" data-line-start="16" data-line-end="17"><a href="https://documenter.getpostman.com/view/14961891/TzJsed6w">school-dropouts</a> (developed by <a href="https://github.com/framacvaz">Francisco Javier Macero Vázquez</a>)</li>
+            <li class="has-line-data" data-line-start="17" data-line-end="18"><a href="https://documenter.getpostman.com/view/14966687/TzJoF1dY">drug_offences</a> (developed by <a href="https://github.com/juaroltor">Juan Roldán Torres</a>)</li>
+            </ul>
+            </li>
+        </ul>
+</main>
