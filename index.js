f53a4b772daf425a43b1d7d4a0d5df758a71ea4e
enrike7lg <enri7luk@hotmail.com>
2021-04-29 23:28:48 +0200

    cambio carpetas

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ var Datastore = require("nedb");
 //Variables
 var app = express();
 var port = (process.env.PORT || 10000);
-var BASE_API_PATH = "/api/v1";
 
 //Server usages
 app.use(bodyParser.json()); 
