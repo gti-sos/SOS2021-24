380b228cfeee81887a5bd0e94840bd81c5eff015
enrike7lg <enri7luk@hotmail.com>
2021-04-29 14:24:55 +0200

    change info

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,3 +1,9 @@
+<script>
+    import {
+      Button,
+    } from "sveltestrap";
+  </script>
+
 <main>
     <head>
         <link rel='stylesheet' href='/global.css'>
@@ -16,7 +22,7 @@
             </ul>
             </li>
             <li class="has-line-data" data-line-start="6" data-line-end="7">
-            <p class="has-line-data" data-line-start="6" data-line-end="7"><strong>Descripción del proyecto</strong>:Correlación entre el número de niños que abandonan la educación primaria,la prevalencia del VIH en jóvenes entre 0-14 años y el porcentaje de niños empleados entre 7-14 años.</p>
+            <p class="has-line-data" data-line-start="6" data-line-end="7"><strong>Descripción del proyecto:</strong> Correlación entre el número de niños que abandonan la educación primaria,la prevalencia del VIH en jóvenes entre 0-14 años y el porcentaje de niños empleados entre 7-14 años.</p>
             </li>
             <li class="has-line-data" data-line-start="7" data-line-end="8">
             <p class="has-line-data" data-line-start="7" data-line-end="8"><strong>Repositorio</strong>: <a href="https://github.com/gti-sos/SOS2021-24">gti-sos/SOS2021-24</a></p>
@@ -25,19 +31,28 @@
             <p class="has-line-data" data-line-start="8" data-line-end="9"><strong>URL</strong>: <a href="https://sos2021-24.herokuapp.com">https://sos2021-24.herokuapp.com</a></p>
             </li>
             <li class="has-line-data" data-line-start="9" data-line-end="14">
-            <p class="has-line-data" data-line-start="9" data-line-end="10"><strong>APIs</strong>:</p>
+            <p class="has-line-data" data-line-start="9" data-line-end="10"><strong>API Backend</strong>:</p>
             <ul>
             <li class="has-line-data" data-line-start="10" data-line-end="11"><a href="https://sos2021-24.herokuapp.com/api/v1/children-out-school">https://sos2021-24.herokuapp.com/api/v1/children-out-school</a> (desarrollado por <a href="https://github.com/enrike7lg">Enrique Luque García</a>)</li>
             <li class="has-line-data" data-line-start="11" data-line-end="12"><a href="https://sos2021-24.herokuapp.com/api/v1/children-employment">https://sos2021-24.herokuapp.com/api/v1/children-employment</a> (desarrollado por <a href="https://github.com/framacvaz">Francisco Javier Macero Vázquez</a>)</li>
-            <li class="has-line-data" data-line-start="12" data-line-end="14"><a href="https://sos2021-24.herokuapp.com/api/v1/children-with-hiv">https://sos2021-24.herokuapp.com/api/v1/children-with-hiv</a> (desarrollado por <a href="https://github.com/juaroltor">Juan Ignacio Roldán Torres</a>)</li>
+            <li class="has-line-data" data-line-start="12" data-line-end="13"><a href="https://sos2021-24.herokuapp.com/api/v1/children-with-hiv">https://sos2021-24.herokuapp.com/api/v1/children-with-hiv</a> (desarrollado por <a href="https://github.com/juaroltor">Juan Ignacio Roldán Torres</a>)</li>
             </ul>
             </li>
             <li class="has-line-data" data-line-start="14" data-line-end="18">
-            <p class="has-line-data" data-line-start="14" data-line-end="15"><strong>Documentación Postman</strong>:</p>
+            <p class="has-line-data" data-line-start="14" data-line-end="15"><strong>API Frontend</strong>:</p>
             <ul>
-            <li class="has-line-data" data-line-start="15" data-line-end="16"><a href="https://documenter.getpostman.com/view/14937153/TzJoDfw2">Children-out-school API Doc</a> (desarrollado por <a  href="https://github.com/enrike7lg">Enrique Luque García</a>)</li>
-            <li class="has-line-data" data-line-start="16" data-line-end="17"> <a href="https://documenter.getpostman.com/view/14961891/TzJsed6w">Children-employment API Doc</a> (desarrollado por <a  href="https://github.com/framacvaz">Francisco Javier Macero Vazquez</a>)</li>
-            <li class="has-line-data" data-line-start="17" data-line-end="18"><a href="https://documenter.getpostman.com/view/14966687/TzJoF1dY">Children-with-HIV API Doc</a> (desarrollado por <a  href="https://github.com/juaroltor">Juan Ignacio Roldán Torres</a>)</li>
+                <li class="has-line-data" data-line-start="15" data-line-end="16"><a href="/#/children-out-school"><Button color="primary">Abandono escolar</Button></a> (desarrollado por <a href="https://github.com/enrike7lg">Enrique Luque García</a>)</li>
+                <li class="has-line-data" data-line-start="16" data-line-end="17"><a href="/#/children-employment">children-employment </a> (desarrollado por <a href="https://github.com/framacvaz">Francisco Javier Macero Vázquez</a>)</li>
+                <li class="has-line-data" data-line-start="17" data-line-end="18"><a href="/#/children-with-hiv">children-with-hiv</a> (desarrollado por <a href="https://github.com/juaroltor">Juan Ignacio Roldán Torres</a>)</li>
+            </ul>
+            </li>
+
+            <li class="has-line-data" data-line-start="18" data-line-end="22">
+            <p class="has-line-data" data-line-start="18" data-line-end="19"><strong>Documentación Postman</strong>:</p>
+            <ul>
+            <li class="has-line-data" data-line-start="19" data-line-end="20"><a href="https://documenter.getpostman.com/view/14937153/TzJoDfw2">Children-out-school API Doc</a> (desarrollado por <a  href="https://github.com/enrike7lg">Enrique Luque García</a>)</li>
+            <li class="has-line-data" data-line-start="20" data-line-end="21"> <a href="https://documenter.getpostman.com/view/14961891/TzJsed6w">Children-employment API Doc</a> (desarrollado por <a  href="https://github.com/framacvaz">Francisco Javier Macero Vazquez</a>)</li>
+            <li class="has-line-data" data-line-start="21" data-line-end="22"><a href="https://documenter.getpostman.com/view/14966687/TzJoF1dY">Children-with-HIV API Doc</a> (desarrollado por <a  href="https://github.com/juaroltor">Juan Ignacio Roldán Torres</a>)</li>
             </ul>
             </li>
         </ul>
