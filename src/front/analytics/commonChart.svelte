52eee8de39841067c7ed55609c3b2261378cf3ba
usuario <juanignacior99@gmail.com>
2021-05-17 20:35:01 +0200

    Retoques

--- src/front/analytics/commonChart.svelte
+++ src/front/analytics/commonChart.svelte
@@ -206,7 +206,7 @@
           Gráfico de líneas básico que muestra la correlación entre:<br> 
           - El total de niños/as que abanandonan la escuela<br>
           - El total de niños/as que están infectados por VIH<br> 
-          - El total de niños/as que están empleados (por cada 100000).
+          - El total de niños/as que están empleados.
         </p>
       </figure>
     {/if}
