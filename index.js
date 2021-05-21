9569f17598f07d1754755e8b853cc25c8776430d
enrike7lg <enri7luk@hotmail.com>
2021-05-21 20:55:36 +0200

    cors

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var port = (process.env.PORT || 10000);
 app.use(bodyParser.json()); 
 app.use(express.json());
 app.use(cors());
+
 app.use("/", express.static(path.join(__dirname,"public")));
 
 //APIs
