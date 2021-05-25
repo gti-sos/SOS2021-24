cce9bce4c2ac8a83632c27b974ab52b25502751e
enrike7lg <enri7luk@hotmail.com>
2021-05-25 22:33:32 +0200

    proxy implemented

--- src/front/Integrations/IntegreationsALL.svelte
+++ src/front/Integrations/IntegreationsALL.svelte
@@ -7,20 +7,20 @@
        
        <h2>Integraciones del grupo SOS2021-24</h2>
        <body>
-        <p>LEYENDA: <Button color= "success">Integrado y con proxy</Button>
-        <Button color= "warning">Integrado</Button>
-        <Button color= "secondary">PROXY</Button>
-        <Button color= "danger">Solo widget</Button></p>
+        <p>(Borrar) LEYENDA:
+        <Button color= "success">Integrado</Button>
+        <Button color= "primary">PROXY</Button>
+        <Button color= "warning">Solo widget</Button></p>
        <p></p>
        <p></p>
        <p></p>
        <h4>Integraciones de Abandono Escolar (Enrique Luque García)</h4>
        <div class="div-home">
-           <p><Button color="warning" href="#/integrations/children-out-school/expenditures">Integracíon Education Expenditures</Button></p>
+           <p><Button color="success" href="#/integrations/children-out-school/expenditures">Integracíon Education Expenditures</Button></p>
            <h6>Integracion APIs Externas</h6> <Button color="danger" href="#/integrations/children-out-school/digimon">Integracíon Ext. Digimon</Button>
-           <Button color="danger" href="#/integrations/children-out-school/yugioh">Integracíon Ext. YU-GI-OH</Button>
-           <Button color="secondary" href="#/integrations/children-out-school/quote">Integracíon Ext. Quotes</Button>
-           <Button color="danger" href="#/integrations/children-out-school/4">Integracíon Ext. 4</Button>
+           <Button color="warning" href="#/integrations/children-out-school/yugioh">Integracíon Ext. YU-GI-OH</Button>
+           <Button color="primary" href="#/integrations/children-out-school/quote">Integracíon Ext. Quotes</Button>
+           <Button color="warning" href="#/integrations/children-out-school/4">Integracíon Ext. 4</Button>
        </div>
        <br>
        <h4>Integraciones del resto</h4>
