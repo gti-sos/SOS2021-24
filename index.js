434270afd63921a070bf0c63fb25581014744e57
usuario <juanignacior99@gmail.com>
2021-06-04 16:30:34 +0200

    prueba

--- index.js
+++ index.js
@@ -47,34 +47,6 @@ app.use(grupo01, function(req,res){
 });
 
 
-//========Proxys Javi============
-//Grupo 23: unemployment-stats
-var api23 = " https://sos2021-23.herokuapp.com";
-var path23 = "/api/v2/unemployment-stats";
-
-app.use(path23, function(req, res) {
-  var url = api23 + req.baseUrl + req.url;
-  console.log('piped: ' + req.baseUrl + req.url);
-  req.pipe(request(url)).pipe(res);
-});
-
-//Grupo 01: divorce-stats
-var api01 = "https://sos2021-01.herokuapp.com";
-var path01 = "/api/v1/divorce-stats";
-app.use(path01, function(req, res) {
-	var url = api01 + req.baseUrl + req.url;
-	console.log('piped: ' + req.baseUrl + req.url);
-	req.pipe(request(url)).pipe(res);
-  });
-  
-
-
-
-
-//==========================
-
-
-
 //========================F02 /cool====================
 app.get("/cool", (request,response) => {
 	response.send(cool());
